cd10a4f9db8713e53ad9bb9241c28cedd9aab8f8
eduvianna <eduardocosta_viana@live.com>
2020-01-20 15:24:25 -0300

    Adicionado controllers em suas devidas rotas

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import MeasurementController from './app/controllers/MeasurementController';
+import SensorController from './app/controllers/SensorController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ status: 'ok' });
-});
+routes.post('/create-sensor', SensorController.store);
+routes.put('/update-sensor', SensorController.update);
+
+routes.post('/create-measurement', MeasurementController.store);
 
 export default routes;
