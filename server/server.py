11e15ab155deef097e22dd5fbcd999c95b991c06
Eduardo Viana <eduardocosta_viana@live.com>
2020-04-13 16:17:26 -0300

    Configurações Servidor Python para Docker

---
+++ server/server.py
@@ -0,0 +1,16 @@
+import requests
+import serial
+
+ser = serial.Serial('dev/ttyACM0', 9600)
+while True:
+	read_serial = ser.readline()
+
+	message = read_serial.split(':')
+
+	if(len(message) == 3):
+		id = message[0]
+		word = message[1]
+		value = message[2]
+		value = value.decode().strip()
+		r = requests.post('http://localhost:3333/create-measurement', json={"sensor_id":id, "word": word, "value": value})
+		print r.status_code
