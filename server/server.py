01b16b6b68697edbc8a8dcca6958336bf510744a
Eduardo Viana <eduardocosta_viana@live.com>
2020-04-14 17:12:10 -0300

    Last Configs

--- server/server.py
+++ server/server.py
@@ -3,21 +3,8 @@ import serial
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 while True:
-<<<<<<< HEAD
-	read_serial = ser.readline()
-	print read_serial
-	#message = read_serial.split(':')
-
-	#if(len(message) == 3):
-	#	id = message[0]
-	#	word = message[1]
-	#	value = message[2]
-	#	value = value.decode().strip()
-	#	r = requests.post('http://localhost:3333/create-measurement', json={"sensor_id":id, "word": word, "value": value})
-	#	print r.status_code
-=======
     read_serial = ser.readline()
-    
+
     message = read_serial.split(':')
 
     if len(message) == 3:
@@ -33,4 +20,3 @@ while True:
             print r.status_code
         except requests.exceptions.ConnectionError:
             print 'Connection refused'
->>>>>>> c4ca75581f65e6570afffe9b6320f6469f5ad615
