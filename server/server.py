08054035d54d7ae05c0bb1fcaebe7b32e487963e
Eduardo Viana <eduardocosta_viana@live.com>
2020-04-14 17:07:51 -0300

    Last Configs

--- server/server.py
+++ server/server.py
@@ -1,16 +1,16 @@
 import requests
 import serial
 
-ser = serial.Serial('dev/ttyACM0', 9600)
+ser = serial.Serial('/dev/ttyACM0', 9600)
 while True:
 	read_serial = ser.readline()
+	print read_serial
+	#message = read_serial.split(':')
 
-	message = read_serial.split(':')
-
-	if(len(message) == 3):
-		id = message[0]
-		word = message[1]
-		value = message[2]
-		value = value.decode().strip()
-		r = requests.post('http://localhost:3333/create-measurement', json={"sensor_id":id, "word": word, "value": value})
-		print r.status_code
+	#if(len(message) == 3):
+	#	id = message[0]
+	#	word = message[1]
+	#	value = message[2]
+	#	value = value.decode().strip()
+	#	r = requests.post('http://localhost:3333/create-measurement', json={"sensor_id":id, "word": word, "value": value})
+	#	print r.status_code
