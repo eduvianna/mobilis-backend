0986afb7e86db73fcd4f2b65ceb8409679b46968
Eduardo Viana <eduardocosta_viana@live.com>
2020-04-14 21:50:57 -0300

    Arrumado Servidor Python

--- arduino_codes/server.py
+++ arduino_codes/server.py
@@ -1,16 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import requests
 import serial
+import serial.tools.list_ports
 
-ser = serial.Serial('dev/ttyACM0', 9600)
+# ser = serial.Serial('dev/ttyACM0', 9600)
+
 while True:
-	read_serial = ser.readline()
-	
-	message = read_serial.split(':')
-	
-	if(len(message) == 3):
-		id = message[0]
-		word = message[1]
-		value = message[2]
-		value = value.decode().strip()
-		r = requests.post('http://localhost:3333/create-measurement', json={"sensor_id":id, "word": word, "value": value})
-		print r.status_code
+    ports = list(serial.tools.list_ports.comports())
+
+    for p in ports:
+        p = str(p)
+        p = p.split('-')
+
+        if 'ttyACM' in p[1]:
+            print p
+
+            read_serial = ser.readline()
+
+            message = read_serial.split(':')
+
+            if(len(message) == 3):
+                id = message[0]
+                word = message[1]
+                value = message[2]
+                value = value.decode().strip()
+                r = requests.post('http://localhost:3333/create-measurement', json={"sensor_id":id, "word": word, "value": value})
+              print r.status_code
+
--- server/server.py
+++ server/server.py
@@ -1,22 +1,33 @@
 import requests
 import serial
+import serial.tools.list_ports
 
-ser = serial.Serial('/dev/ttyACM0', 9600)
+
 while True:
-    read_serial = ser.readline()
+    ports = list(serial.tools.list_ports.comports())
 
-    message = read_serial.split(':')
+    for port in ports:
+        port = str(port)
+        port = port.split('-')
+        port[0] = port[0].strip()
+        if 'ttyACM' in port[1]:
 
-    if len(message) == 3:
-        try:
-            id = message[0]
-            word = message[1]
-            value = message[2]
-            value = value.decode().strip()
-            r = \
-                requests.post('http://app-mobilis:3333/create-measurement'
-                              , json={'sensor_id': id, 'word': word,
-                              'value': value})
-            print r.status_code
-        except requests.exceptions.ConnectionError:
-            print 'Connection refused'
+          ser = serial.Serial(port[0], 9600)
+
+          read_serial = ser.readline()
+
+          message = read_serial.split(':')
+
+          if len(message) == 3:
+              try:
+                  id = message[0]
+                  word = message[1]
+                  value = message[2]
+                  value = value.decode().strip()
+                  r = \
+                      requests.post('http://app-mobilis:3333/create-measurement'
+                                    , json={'sensor_id': id, 'word': word,
+                                    'value': value})
+                  print r.status_code
+              except requests.exceptions.ConnectionError:
+                  print 'Connection refused'
